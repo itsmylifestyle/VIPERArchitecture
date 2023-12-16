cef74cb69445ec867d0cfb9e4f4d030a2bf4c0ef
itsmylifestyle <hahaii00@mail.ru>
2023-12-16 22:52:38 +0600

    View

--- VIPER/View.swift
+++ VIPER/View.swift
@@ -11,9 +11,70 @@ import UIKit
 //Class, protocol, 
 
 protocol AnyView {
+    var presenter : AnyPresenter? {get set}
     
+    func update(with cryptos: [crypto])
+    func update(with error: String)
 }
 
-class CryptoViewContoller : UIViewController, AnyView {
+class CryptoViewContoller : UIViewController, AnyView, UITableViewDelegate, UITableViewDataSource {
+    
+    var cryptos : [crypto] = []
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cryptos.count
+    }
+    
+    private let tableView : UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
+    private let messageLabel : UILabel = {
+        let label = UILabel()
+        label.isHidden = false
+        label.text = "Downloading..."
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .black
+        label.textAlignment = .center
+        
+        return label
+        
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        view.addSubview(messageLabel)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        messageLabel.frame = CGRect(x: view.frame.width / 2 - 100, y: view.frame.height / 2 - 25, width: 200, height: 50)
+    }
+    
+    func update(with cryptos: [crypto]) {
+        DispatchQueue.main.async {
+            self.cryptos = cryptos
+            self.messageLabel.isHidden = true
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+        }
+    }
+    
+    func update(with error: String) {
+        
+    }
+    
+    var presenter: AnyPresenter?
     
 }
